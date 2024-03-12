a3cc5075b42c23eae3abfe9e0de65155e0ee1df6
Luke <lskerret@ucsd.edu>
2024-03-11 23:15:44 -0700

    organization rehaul

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Navbar from './Navbar';
+import Navbar from './components/Navbar/component';
 import './App.css';
-import Video from './video.mp4'
+import Video from './assets/video.mp4'
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <header className="App-header">
-
-
         <video src={Video} width='700px' height='400px'controls>
         </video>
         <footer className='App-footer'>
---
+++ src/components/Navbar/component.jsx
@@ -0,0 +1,17 @@
+import Car from '../../assets/Car.png';
+import './component.css'
+export default function Navbar () {
+    return (
+        <div className="navbar">
+            <nav>
+                <a href='https://www.temu.com/'> <img src = {Car} alt='temu logo'></img> </a>
+                <ul>
+                    <li>
+                        
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    );
+}
