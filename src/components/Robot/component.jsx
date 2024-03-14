49d168485d11fee19d946d090045b95a0c4a21c1
Luke <lskerret@ucsd.edu>
2024-03-13 19:08:35 -0700

    alt

--- src/components/Robot/component.jsx
+++ src/components/Robot/component.jsx
@@ -3,7 +3,7 @@ import './component.css'
 export default function Robot(){
     return(
         <div>
-            <img src={Car} className='final--car'></img>
+            <img src={Car} alt='diagram of robot car'className='final--car'></img>
         </div>
     )
 }
